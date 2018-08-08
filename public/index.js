1c658167b82336ca68fa87ac16bdb1a38f10021a
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2018-08-08 18:58:25 +0900

    Use ObservableTodoStore

--- public/index.js
+++ public/index.js
@@ -1,16 +1,7 @@
-import todoStore from './TodoStore';
+import observableTodoStore from './ObservableTodoStore';
 
-todoStore.addTodo('read MobX tutorial');
-console.log(todoStore.report());
-
-todoStore.addTodo('try MobX');
-console.log(todoStore.report());
-
-todoStore.todos[0].completed = true;
-console.log(todoStore.report());
-
-todoStore.todos[1].task = 'try MobX in own project';
-console.log(todoStore.report());
-
-todoStore.todos[0].task = 'grok MobX tutorial';
-console.log(todoStore.report());
+observableTodoStore.addTodo('read MobX tutorial');
+observableTodoStore.addTodo('try MobX');
+observableTodoStore.todos[0].completed = true;
+observableTodoStore.todos[1].task = 'try MobX in own project';
+observableTodoStore.todos[0].task = 'grok MobX tutorial';
